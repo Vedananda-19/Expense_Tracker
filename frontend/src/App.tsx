f5ab9831ace2888f3dd4b257463c7f07f178dc0b
Vedananda-19 <pvedananda@gmail.com>
2026-06-07 19:11:31 +0530

    Fixed Deployment Bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,32 @@
-import {createBrowserRouter,RouterProvider} from "react-router-dom"
-import TrackerPage from "../pages/TrackerPage"
-import AnalyticsPage from "../pages/AnalyticsPage"
-import MainLayout from "../Layouts/MainLayout"
-import {TxnContextProvider} from "../Context/TxnContextProvider"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TrackerPage from "../pages/TrackerPage";
+import AnalyticsPage from "../pages/AnalyticsPage";
+import MainLayout from "../Layouts/MainLayout";
+import { TxnContextProvider } from "../Context/TxnContextProvider";
 
-const router = createBrowserRouter([
-  {
-    element:<MainLayout />,
-    children:[
-      {path:"/",element:<TrackerPage />},
-      {path:"/analytics",element:<AnalyticsPage />}
-    ]
-  }
-])
+const router = createBrowserRouter(
+    [
+        {
+            element: <MainLayout />,
+            children: [
+                { path: "/", element: <TrackerPage /> },
+                { path: "/analytics", element: <AnalyticsPage /> },
+            ],
+        },
+    ],
+    {
+        basename: "/Expense_Tracker",
+    },
+);
 
-function App(){
-    return(
-      <>
-        <TxnContextProvider>
-          <RouterProvider router={router} />
-        </TxnContextProvider>
-      </>
-    )
+function App() {
+    return (
+        <>
+            <TxnContextProvider>
+                <RouterProvider router={router} />
+            </TxnContextProvider>
+        </>
+    );
 }
 
-export default App
+export default App;
